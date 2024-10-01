650026be3e9f288f6c3147dd8fde3f4eb161bf16
Ana Luiza da Silva Fonseca <141242999+AnaLuizaFonseca@users.noreply.github.com>
2024-10-01 16:54:00 -0300

    Delete Biblioteca Inteligente/Biblioteca directory

--- Biblioteca Inteligente/Biblioteca/inicio.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    spaceBetween: 10,
-    loop: true,
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-    }
-    });
